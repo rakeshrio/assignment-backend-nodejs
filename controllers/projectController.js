3deb6edf8fef40b9f670bcb4bb2a65bb5d4bd293
rakeshrio <49908435+rakeshrio@users.noreply.github.com>
2021-08-13 23:56:58 +0530

    'added'

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -4,22 +4,23 @@ const router = express.Router();
 
 router.post('/' , async(req , res) => {
     try{
-        const { error } = validate(req.body); 
+        const { error } = validate(req.body);
         if (error) return res.status(400).send({"err": 1 , "message" : error.details[0].message});
 
         let project = new Project({
             name: req.body.name,
             summary: req.body.summary,
+            description: req.body.description
         })
 
         await project.save().then((data) => {
-            return res.status(200).send(data); 
+            return res.status(200).send(data);
         }).catch((err) => {
             console.log(err)
-            return res.send({"err": 1 , "message" : "There was some error adding project."}); 
+            return res.send({"err": 1 , "message" : "There was some error adding project."});
         })
     }catch(e){
-        return res.status(500).send({"err": 1 , "message" : "Internal Server Error"}); 
+        return res.status(500).send({"err": 1 , "message" : "Internal Server Error"});
     }
 })
 
@@ -40,12 +41,12 @@ router.put('/:id' , async(req , res) => {
 
         await Project.updateOne({_id : id} , {$set : req.body})
             .then((data) => {
-                return res.send({"err": 0 , "message" : "Updated successfully"}); 
+                return res.send({"err": 0 , "message" : "Updated successfully"});
             }).catch((err) => {
-                return res.send({"err": 1 , "message" : "Error while updating."}); 
+                return res.send({"err": 1 , "message" : "Error while updating."});
             })
     }catch(e){
-        return res.status(500).send({"err": 1 , "message" : "Internal Server Error"}); 
+        return res.status(500).send({"err": 1 , "message" : "Internal Server Error"});
     }
 })
 
@@ -56,20 +57,20 @@ router.delete('/:id' , async (req , res) => {
             return res.send({"err": 1 , "message" : "Provided ID is not valid"});
         }
         await Project.deleteOne({_id : id}).then((data) => {
-            return res.send({"err": 0 , "message" : "Deleted successfully"}); 
+            return res.send({"err": 0 , "message" : "Deleted successfully"});
         }).catch((err) => {
-            return res.send({"err": 1 , "message" : "Error in model deletion"}); 
+            return res.send({"err": 1 , "message" : "Error in model deletion"});
         })
 
     }catch(e){
-        return res.status(500).send({"err": 1 , "message" : "Internal Server Error"}); 
+        return res.status(500).send({"err": 1 , "message" : "Internal Server Error"});
     }
 })
 
 router.post('/data' , async(req , res) => {
     try {
         // get the query params
-    
+
         const { q, limit, offset } = req.query;
         const sort = req.body.sort
         let query = {}
--- models/project.js
+++ models/project.js
@@ -5,6 +5,7 @@ const Joi      = require('joi');
 const projectSchema = new mongoose.Schema({
     name    : String,
     summary   : String,
+    description : String,
     createdAt: { type:Date, default:Date.now},
 });
 const projectModal = mongoose.model('projects', projectSchema)
@@ -13,10 +14,11 @@ function projectModalValidate(modal) {
     const schema = {
         name    : Joi.string().required(),
         summary   : Joi.string().required(),
+        description : Joi.string().required(),
     };
-  
+
     return Joi.validate(modal, schema);
   }
-  
+
 module.exports.Project   = projectModal;
 module.exports.validate = projectModalValidate;
